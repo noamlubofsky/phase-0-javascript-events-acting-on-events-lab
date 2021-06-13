dc3a3de5f89a0f8620f628330ab08427a9cf157c
github username <github email address>
2021-06-13 17:50:17 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 // Your code here
 const dodger = document.getElementById("dodger"); 
 
+dodger.style.backgroundColor = "#FF69B4"; 
+
+dodger.style.bottom = "0px"; 
+
+
 
 function moveDodgerLeft() {
   const leftNumbers = dodger.style.left.replace("px", "");
@@ -22,7 +27,16 @@ function moveDodgerRight() {
     let leftNumbers = dodger.style.left.replace("px", "");
     let left = parseInt(leftNumbers, 10);
   
-    if (left > 0) {
+    if (left < 360) {
       dodger.style.left = `${left + 1}px`;
     }
   }
+
+  document.addEventListener("keydown", function(e) {
+    if (e.key === "ArrowRight") {
+      moveDodgerRight();
+    }
+  }); 
+
+
+  
